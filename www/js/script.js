20bd98f3e9e89b78a8ff96afaa78cc3dbf0e0c61
Kirill <12ab8192>
2019-02-22 20:31:05 +0300

    index page finished

--- www/js/script.js
+++ www/js/script.js
@@ -3,16 +3,28 @@ $(document).ready(function() {
 	$('#comment__slider').owlCarousel({
 		loop: true,
 		center: true,
-		nav: true,
 		navText: ['<img src="img/arrowl.png">', '<img src="img/arrowr.png">'],
 		items: 3,
 		autoplay: true,
-		paginationSpeed: 400
+		autoWidth: true,
+		paginationSpeed: 400,
+		responsive:{
+	        0:{
+	            items:1,
+	            nav: false,
+	        },
+	        600:{
+	            items:1,
+	        },
+	        1000:{
+	            items:1,
+	            nav: false,
+	        },
+	        1200: {
+	        	nav: true,
+	        	items: 3,
+	        }
+	    }
 	});
-
-	let slider = document.getElementById('comment__slider');
-	let centerElement = document.getElementsByClassName('center');
-	centerElement.style.display = 'none'
-	//slider.style.height = 
 
 });
